59170cffcc3581628779b02a89f0291e9a20511a
Rafael Tavares <rafael+github@tavares.email>
2018-01-04 12:13:34 -0800

    Redeclares modules as path definitions (#6)

--- material-ui-pagination/index.d.ts
+++ material-ui-pagination/index.d.ts
@@ -1,19 +1,18 @@
 /// <reference types="react" />
 
-declare module 'material-ui-pagination' {
-  import * as React from 'react';
+import * as React from 'react';
 
-  export interface PaginationProps {
-    current: number;
-    display: number;
-    onChange(value: number): void;
-    styleButton?: React.CSSProperties;
-    styleFirstPageLink?: React.CSSProperties;
-    styleLastPageLink?: React.CSSProperties;
-    stylePrimary?: React.CSSProperties;
-    styleRoot?: React.CSSProperties;
-    total: number;
-  }
-
-  export default class Pagination extends React.Component<PaginationProps> {}
+export interface PaginationProps {
+  current: number;
+  display: number;
+  onChange(value: number): void;
+  styleButton?: React.CSSProperties;
+  styleFirstPageLink?: React.CSSProperties;
+  styleLastPageLink?: React.CSSProperties;
+  stylePrimary?: React.CSSProperties;
+  styleRoot?: React.CSSProperties;
+  total: number;
 }
+
+declare const Pagination: React.ComponentClass<PaginationProps>;
+export default Pagination;
--- react-text-mask/index.d.ts
+++ react-text-mask/index.d.ts
@@ -1,41 +1,43 @@
 /// <reference types="react" />
 
-declare module 'react-text-mask' {
-  import * as React from 'react';
+import * as React from 'react';
 
-  namespace TextMask {
-    export type SupportedTypes = 'text' | 'tel' | 'url' | 'password' | 'search';
-    export type Mask = (string | RegExp)[];
-  }
-
-  export type Mask = TextMask.Mask;
-  export type SupportedTypes = TextMask.SupportedTypes;
-
-  type PipeFactory = (conformedValue?: string, config?: Config) => false | string | object;
-
-  export interface Config {
+declare namespace TextMask {
+  interface Config {
     currentCaretPosition: number;
     guide: boolean;
     keepCharPositions: boolean;
-    pipe: PipeFactory,
+    pipe: TextMask.PipeHandlerFn,
     placeholder: string;
     placeholderChar: string;
     previousConformedValue: string;
     rawValue: string;
   }
 
-  export type MaskHandlerFn = (rawValue: string) => Mask;
+  export type Mask = (string | RegExp)[];
 
-  export interface TextMaskProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    guide?: boolean;
-    keepCharPositions?: boolean;
-    mask: Mask | MaskHandlerFn;
-    pipe?: PipeFactory;
-    placeholderChar?: string;
-    showMask?: boolean;
-    type?: SupportedTypes;
-  }
+  export type PipeHandlerFn = (conformedValue?: string, config?: Config) => false | string | object;
 
-  const ReactTextMask: React.ComponentClass<TextMaskProps>;
-  export default ReactTextMask;
+  export type SupportedTypes = 'text' | 'tel' | 'url' | 'password' | 'search';
 }
+
+export type Mask = TextMask.Mask;
+
+export type PipeHandlerFn = TextMask.PipeHandlerFn;
+
+export type SupportedTypes = TextMask.SupportedTypes;
+
+export type MaskHandlerFn = (rawValue: string) => Mask;
+
+export interface TextMaskProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  guide?: boolean;
+  keepCharPositions?: boolean;
+  mask: Mask | MaskHandlerFn;
+  pipe?: TextMask.PipeHandlerFn;
+  placeholderChar?: string;
+  showMask?: boolean;
+  type?: SupportedTypes;
+}
+
+declare const ReactTextMask: React.ComponentClass<TextMaskProps>;
+export default ReactTextMask;
