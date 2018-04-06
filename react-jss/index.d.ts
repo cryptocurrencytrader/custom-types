5db3d918f381b119d4ff2e59c431f14eddfe9d72
Rafael Tavares <rafael+github@tavares.email>
2018-04-06 08:52:27 -0700

    Updates react-jss type definitions (#9)

--- react-jss/index.d.ts
+++ react-jss/index.d.ts
@@ -1,6 +1,6 @@
-/// <reference types="react"/>
-
 import * as React from 'react';
+import * as CSS from 'csstype';
+import { Omit } from 'type-zoo';
 
 interface JSS {
   use: (...plugins: Array<() => any>) => void;
@@ -12,6 +12,33 @@ export interface InjectSheetProps<C extends string> {
   classes: { [key in C]: string; };
 }
 
+type MapType = string | number;
+type CSSPropertiesMap = CSS.Properties<MapType>;
+
+type CSSRecord<K extends keyof CSSPropertiesMap, T> = Record<K, Pick<CSSPropertiesMap, K>[K]> | Record<K, T>;
+
+type EnhancedProperties = (
+  CSSRecord<'animation', Array<any[]>> &
+  CSSRecord<
+    'backgroundSize' | 'backgroundPosition' | 'border' | 'borderBottom' | 'borderLeft' |
+    'borderTop' | 'borderRight' | 'boxShadow' | 'flex' | 'margin' | 'padding' | 'outline' |
+    'transformOrigin' | 'transform' | 'transition',
+    any[]
+  >
+);
+
+type FlattenEnhancedProperties = {
+  [key in keyof EnhancedProperties]?: EnhancedProperties[key];
+};
+
+export interface CSSProperties extends Omit<CSSPropertiesMap, keyof FlattenEnhancedProperties>, FlattenEnhancedProperties {
+  [key: string]: any;
+}
+
+export interface Styles {
+  [key: string]: CSSProperties;
+}
+
 type ComponentConstructor<P> = React.ComponentClass<P> | React.SFC<P>;
 
 interface ComponentWithStyles<InjectedProps, C extends string> {
@@ -20,6 +47,4 @@ interface ComponentWithStyles<InjectedProps, C extends string> {
   ): React.ComponentClass<P & InjectSheetProps<C>>;
 }
 
-export default function injectSheet<P, C extends string>(
-  styles: { [key: string]: React.CSSProperties }
-): ComponentWithStyles<P, C>;
+export default function injectSheet<P, C extends string>(styles: Styles): ComponentWithStyles<P, C>;
