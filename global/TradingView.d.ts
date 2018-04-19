984c4b8cd7cb139fc533af03c4cb1b411605f40e
Dennis Alves <dennis.alves@live.com>
2018-04-19 16:11:05 -0300

    Added tradingview missing types

--- global/TradingView.d.ts
+++ global/TradingView.d.ts
@@ -20,23 +20,36 @@ declare namespace TradingViewModule {
     client_id: string;
     container_id: string;
     datafeed: any;
+    debug?: boolean;
     disabled_features: string[];
     drawings_access: DrawingAccess;
     enabled_features: string[];
     fullscreen?: boolean;
+    height?: number;
     interval: string;
     library_path?: string;
     locale: string;
+    overrides?: { [key: string]: string };
     symbol: string;
+    timeframe?: string;
+    time_frames?: TimeFrame[];
+    width?: number;
     user_id: string;
+  }
+
+  interface DrawingAccess {
+    type: string;
+    tools: ToolDescription[];
   }
 
   interface ToolDescription {
     name: string;
   }
 
-  interface DrawingAccess {
-    type: string;
-    tools: ToolDescription[];
+  interface TimeFrame {
+    text: string;
+    resolution: string;
+    description?: string;
+    title?: string;
   }
 }
