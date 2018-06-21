8ae7b0a9e530b002f598f668bc20e27f2eb2fa91
Rafael Tavares <rafael+github@tavares.email>
2018-06-21 13:51:40 -0300

    React JSS definitions improvements (#15)

--- react-jss/index.d.ts
+++ react-jss/index.d.ts
@@ -1,50 +1,62 @@
-import * as React from 'react';
-import * as CSS from 'csstype';
-import { Omit } from 'type-zoo';
+declare module 'react-jss' {
+  import * as React from 'react';
+  import * as CSS from 'csstype';
 
-interface JSS {
-  use: (...plugins: Array<() => any>) => void;
+  interface JSS {
+    use: (...plugins: Array<() => any>) => void;
+  }
+
+  export const jss: JSS;
+
+  export interface InjectSheetProps<C extends string> {
+    classes: Record<C, string>;
+  }
+
+  export type ThemeProvider = React.ComponentType<{ theme: object | (() => object) }>;
+  export const ThemeProvider: ThemeProvider;
+
+  export interface Theming {
+    ThemeProvider: ThemeProvider;
+  }
+
+  export function createTheming(namespace: string): Theming;
+
+  type MapType = string | number;
+  type CSSPropertiesMap = CSS.Properties<MapType>;
+
+  type CSSRecord<K extends keyof CSSPropertiesMap, T> = Record<K, Pick<CSSPropertiesMap, K>[K]> | Record<K, T>;
+
+  type EnhancedProperties = (
+    CSSRecord<'animation', Array<any[]>> &
+    CSSRecord<
+      'backgroundSize' | 'backgroundPosition' | 'border' | 'borderBottom' | 'borderLeft' |
+      'borderTop' | 'borderRight' | 'boxShadow' | 'flex' | 'margin' | 'padding' | 'outline' |
+      'transformOrigin' | 'transform' | 'transition',
+      any[]
+    >
+  );
+
+  type FlattenEnhancedProperties = {
+    [key in keyof EnhancedProperties]?: EnhancedProperties[key];
+  };
+
+  export interface CSSProperties extends Pick<CSSPropertiesMap, Exclude<keyof CSSPropertiesMap, keyof FlattenEnhancedProperties>>, FlattenEnhancedProperties {
+    [key: string]: any;
+  }
+
+  export type StaticStyles<K extends string = string> = Record<K, CSSProperties>;
+  export type ThemedStyles<T extends object = {}, K extends string = string> = (theme: T) => StaticStyles<K>;
+
+  interface ComponentWithStyles<InjectedProps, C extends string> {
+    <P extends InjectedProps>(
+      component: React.ComponentType<P & InjectSheetProps<C>>
+    ): React.ComponentClass<P & InjectSheetProps<C>>;
+  }
+
+  export interface InjectSheetOptions {
+    inject?: Array<'classes' | 'theme' | 'sheet'>
+    theming?: Theming;
+  }
+
+  export default function injectSheet<P, C extends string>(styles: StaticStyles | ThemedStyles, options?: InjectSheetOptions): ComponentWithStyles<P, C>;
 }
-
-export const jss: JSS;
-
-export interface InjectSheetProps<C extends string> {
-  classes: { [key in C]: string; };
-}
-
-type MapType = string | number;
-type CSSPropertiesMap = CSS.Properties<MapType>;
-
-type CSSRecord<K extends keyof CSSPropertiesMap, T> = Record<K, Pick<CSSPropertiesMap, K>[K]> | Record<K, T>;
-
-type EnhancedProperties = (
-  CSSRecord<'animation', Array<any[]>> &
-  CSSRecord<
-    'backgroundSize' | 'backgroundPosition' | 'border' | 'borderBottom' | 'borderLeft' |
-    'borderTop' | 'borderRight' | 'boxShadow' | 'flex' | 'margin' | 'padding' | 'outline' |
-    'transformOrigin' | 'transform' | 'transition',
-    any[]
-  >
-);
-
-type FlattenEnhancedProperties = {
-  [key in keyof EnhancedProperties]?: EnhancedProperties[key];
-};
-
-export interface CSSProperties extends Omit<CSSPropertiesMap, keyof FlattenEnhancedProperties>, FlattenEnhancedProperties {
-  [key: string]: any;
-}
-
-export type Styles<K extends string = string> = {
-  [key in K]: CSSProperties;
-}
-
-type ComponentConstructor<P> = React.ComponentClass<P> | React.SFC<P>;
-
-interface ComponentWithStyles<InjectedProps, C extends string> {
-  <P extends InjectedProps>(
-    component: ComponentConstructor<P & InjectSheetProps<C>>
-  ): React.ComponentClass<P & InjectSheetProps<C>>;
-}
-
-export default function injectSheet<P, C extends string>(styles: Styles): ComponentWithStyles<P, C>;
